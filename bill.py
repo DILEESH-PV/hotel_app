d24955ad717f5cad5b62a0033c13283e2713ce44
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-03 10:53:33 +0530

    added bill option

--- bill.py
+++ bill.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 mydb=mysql.connector.connect(host='localhost',user='root',password='',database='hoteldb')
 mycursor=mydb.cursor()
 total=0
@@ -18,28 +19,28 @@ while(True):
         print(" selected Add Tea option ")
         qty=int(input("Enter the quantity"))
         total+=10*qty
-        item.append("Tea x"+str(qty))
+        item.append("Tea x "+str(qty))
         #print("quantity= ",qty)
         #print("total=",total)
     elif(ch==2):
         print(" selected Add Coffee option")
         qty=int(input("enter the quantity"))
         total+=15*qty
-        item.append("Coffee x"+str(qty))
+        item.append("Coffee x "+str(qty))
         #print("quantity= ",qty)
         #print("total=",total)
     elif(ch==3):
         print("selected Add Burger option")
         qty=int(input("enter the quantity"))
         total+=40*qty
-        item.append("Coffee x"+str(qty))
+        item.append("burger x "+str(qty))
         #print("quantity= ",qty)
         #print("total=",total)
     elif(ch==4):
         print("selected Add Sandwich option")
         qty=int(input("enter the quantity"))
         total+=60*qty
-        item.append("Coffee x"+str(qty))
+        item.append("Sandwich x "+str(qty))
         #print("quantity= ",qty)
         #print("total=",total)
         
@@ -47,10 +48,30 @@ while(True):
         print("selected Add Alpham option")
         qty=int(input("enter the quantity"))
         total+=170*qty
-        item.append("Coffee x"+str(qty))
-        print("quantity= ",qty)
-        print("total=",total)
+        item.append("Alpham x   "+str(qty))
+        #print("quantity= ",qty)
+        #print("total=",total)
     elif(ch==6):
-        print("Generating bill")
+        print(" selected Generating bill option")
+        name=input("Enter name")
+        phn=input("Enter the phone number")
+        print("========= BILL========")
+        print("\n Name  : ",name)
+        print("\n Mob   : ",phn)
+        date=datetime.today().strftime('%Y-%m-%d')
+        print("\n Date  : ",date)
+        print("\n============================")
+        print("\n Orderd items")
+        for i in item:
+            print(i)
+        print("\nTotal amount = ",total)
+        print("\n Thank you visit again\n\n")
+        sql="INSERT INTO `bill`(`name`, `phno`, `amout`, `date`) VALUES (%s,%s,%s,now())"
+        data=(name,phn,total)
+        mycursor.execute(sql,data)
+        mydb.commit()
+        print("data inserted successfully on the database")
+        total=0
+        item=[]        
     elif(ch==7):
         break
